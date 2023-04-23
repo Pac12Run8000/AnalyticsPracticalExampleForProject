14b875dc0b6e781453c090f2056012b358316a65
Pac12Run8000 <gangstarr78992@>
2023-04-23 12:22:49 -0700

    Initial Commit

---
+++ AnalyticsPracticalExampleForProject/AnalyticsPracticalExampleForProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyticsPracticalExampleForProjectApp.swift
+//  AnalyticsPracticalExampleForProject
+//
+//  Created by Michelle Grover on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct AnalyticsPracticalExampleForProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
